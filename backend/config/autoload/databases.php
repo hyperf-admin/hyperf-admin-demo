308c6b253c05d6c5d7314ac33a1fe58a19a5106a
刀刀 <wangwenjie@innotechx.com>
2020-06-28 22:31:32 +0800

    fix: add test db

--- backend/config/autoload/databases.php
+++ backend/config/autoload/databases.php
@@ -3,30 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'default' => [
-        'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'port' => env('DB_PORT', 3306),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-        'prefix' => env('DB_PREFIX', ''),
-        'pool' => [
-            'min_connections' => 1,
-            'max_connections' => 10,
-            'connect_timeout' => 10.0,
-            'wait_timeout' => 3.0,
-            'heartbeat' => -1,
-            'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
-        ],
-        'commands' => [
-            'gen:model' => [
-                'path' => 'app/Model',
-                'force_casts' => true,
-                'inheritance' => 'Model',
-            ],
-        ],
-    ],
+    'local' => db_complete([
+        'host' => env('LOCAL_DB_HOST', env('HYPERF_ADMIN_DB_HOST')),
+        'database' => env('LOCAL_DB_NAME', 'test'),
+        'username' => env('LOCAL_DB_USER', env('HYPERF_ADMIN_DB_USER')),
+        'password' => env('LOCAL_DB_PWD', env('HYPERF_ADMIN_DB_PWD')),
+    ]),
 ];
